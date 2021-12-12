1e80387ce916c21133ca3d682779c27f8a24605c
Li-yao Xia <lysxia@gmail.com>
2021-12-12 14:52:21 -0500

    Reorder definitions in Day 12

--- src/aoc12.v
+++ src/aoc12.v
@@ -6,6 +6,8 @@ From stdpp Require Import gmap.
 Set Implicit Arguments.
 Set Contextual Implicit.
 
+(* Parsing *)
+
 Declare Custom Entry edge.
 
 Notation "'input' e1 .. en" := (cons e1 .. (cons en nil) ..)
@@ -25,6 +27,8 @@ Definition example : Input := input
 "A"-"end"
 "b"-"end"
 .
+
+(* Graphs *)
 
 Section G.
 
@@ -50,33 +54,7 @@ End G.
 Arguments graph : clear implicits.
 Arguments graph V {_ _}.
 
-Definition isSome {A} (x : option A) :=
-  match x with
-  | Some _ => true
-  | None => false
-  end.
-
-(* [gset] insertion (as [fun x s => union (singleton x) s]) is slow,
-   so we redefine our own sets from maps which have fast [insert]. *)
-Notation set X := (gmap X unit).
-Notation insert_set x s := (insert x tt s).
-Notation elem_set x s := (isSome (lookup x s)).
-
-Notation v := string (only parsing).
-
-Definition is_big (x : v) : bool :=
-  match x with
-  | ""%string => false
-  | String c _ => ((N_of_ascii "A" <=? N_of_ascii c) && (N_of_ascii c <=? N_of_ascii "Z"))
-  end.
-
-Definition visit (x : v) (visited : set v) : option (set v) :=
-  if is_big x then Some visited
-  else if elem_set x visited then None
-  else Some (insert_set x visited).
-
-Definition sum_N (xs : list N) : N :=
-  fold_left N.add xs 0.
+(* Recursion and memoization *)
 
 Definition endo (X : Type) : Type := (X -> X).
 
@@ -97,16 +75,6 @@ Fixpoint traverse {M} `{Monad M} {A B} (f : A -> M B) (xs : list A) : M (list B)
     let* y := f x in
     let* ys := traverse f xs in
     ret (y :: ys)
-  end.
-
-Definition _count_paths {M} `{Monad M} {VISITED} (_visit : v -> VISITED -> option VISITED)
-  (g : graph v) : endo (VISITED * v -> M N) := fun __count_paths '(visited, x) =>
-  if String.eqb x "end" then ret 1 else
-  match _visit x visited with
-  | None => ret 0
-  | Some visited =>
-    let* ns := traverse (fun x => __count_paths (visited, x)) (get_edges x g) in
-    ret (sum_N ns)
   end.
 
 Record identity (A : Type) : Type := Mk_identity { run_identity : A }.
@@ -147,6 +115,46 @@ Fixpoint _memo_fix (fuel : nat) {A R} `{EqDecision A, Countable A}
 Definition memo_fix (fuel : nat) {A R} `{EqDecision A, Countable A}
   (r0 : R) (gf : endo (A -> state (gmap A R) R)) : A -> R :=
   fun x => snd (run_state (_memo_fix fuel r0 gf x) empty).
+
+(* DFS *)
+
+Definition isSome {A} (x : option A) :=
+  match x with
+  | Some _ => true
+  | None => false
+  end.
+
+(* [gset] insertion (as [fun x s => union (singleton x) s]) is slow,
+   so we redefine our own sets from maps which have fast [insert]. *)
+Notation set X := (gmap X unit).
+Notation insert_set x s := (insert x tt s).
+Notation elem_set x s := (isSome (lookup x s)).
+
+Notation v := string (only parsing).
+
+Definition is_big (x : v) : bool :=
+  match x with
+  | ""%string => false
+  | String c _ => ((N_of_ascii "A" <=? N_of_ascii c) && (N_of_ascii c <=? N_of_ascii "Z"))
+  end.
+
+Definition visit (x : v) (visited : set v) : option (set v) :=
+  if is_big x then Some visited
+  else if elem_set x visited then None
+  else Some (insert_set x visited).
+
+Definition sum_N (xs : list N) : N :=
+  fold_left N.add xs 0.
+
+Definition _count_paths {M} `{Monad M} {VISITED} (_visit : v -> VISITED -> option VISITED)
+  (g : graph v) : endo (VISITED * v -> M N) := fun __count_paths '(visited, x) =>
+  if String.eqb x "end" then ret 1 else
+  match _visit x visited with
+  | None => ret 0
+  | Some visited =>
+    let* ns := traverse (fun x => __count_paths (visited, x)) (get_edges x g) in
+    ret (sum_N ns)
+  end.
 
 (* The maximum length can be up to twice the number of nodes, because we can revisit big nodes.
    An implicit assumption is that there are no adjacent big nodes, so a long path must consist
