73385367b274106303b4329a02df58279b541056
Li-yao Xia <lysxia@gmail.com>
2021-12-12 14:15:54 -0500

    Refactor runall.sh

--- src/runall.sh
+++ src/runall.sh
@@ -1,12 +1,3 @@
-./a.sh aoc01.v solve12 aoc01_input.txt
-./a.sh aoc02.v solve12 aoc02_input.txt
-./a.sh aoc03.v solve12 aoc03_input.txt
-./a.sh aoc04.v solve12 aoc04_input.txt
-./a.sh aoc05.v solve12 aoc05_input.txt
-./a.sh aoc06.v solve12 aoc06_input.txt
-./a.sh aoc07.v solve12 aoc07_input.txt
-./a.sh aoc08.v solve12 aoc08_input.txt
-./a.sh aoc09.v solve12 aoc09_input.txt
-./a.sh aoc10.v solve12 aoc10_input.txt
-./a.sh aoc11.v solve12 aoc11_input.txt
-./a.sh aoc12.v solve12 aoc12_input.txt
+for i in $(seq -f "%02g" 1 12) ; do
+  echo "Day ${i}"; ./a.sh aoc${i}.v solve12 aoc${i}_input.txt
+done
