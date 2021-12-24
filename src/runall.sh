dd9f27f1b6c7fa9e8851e24e518c94b40700eee1
Li-yao Xia <lysxia@gmail.com>
2021-12-24 18:52:11 -0500

    Skip Day 24 in runall

--- src/runall.sh
+++ src/runall.sh
@@ -1,8 +1,11 @@
 for i in $(seq -f "%02g" 1 25) ; do
-  if [ "23" = "${i}" ]
-  then echo "Day ${i} --- SKIPPED"; continue
-  fi
-  if [ -e "aoc${i}.v" ]
-  then echo "Day ${i}"; ./a.sh aoc${i}.v aoc${i}_input.txt solve12
+  if [[ -e "aoc${i}.v" ]] ; then
+    CMD="./a.sh aoc${i}.v aoc${i}_input.txt solve12"
+    if [[ "23" = "${i}"  ]]
+    then echo "Day ${i} --- SKIPPED (Part 2 goes OOM) --- ${CMD}"; continue
+    elif [[ "24" = "${i}" ]]
+    then echo "Day ${i} --- SKIPPED (8 minute wait)   --- ${CMD}"; continue
+    fi
+    echo "Day ${i}"; ${CMD}
   fi
 done
