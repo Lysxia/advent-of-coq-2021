f36b2edf159acfad2f4fc207a712faefcf3af891
Li-yao Xia <lysxia@gmail.com>
2021-12-24 00:39:23 -0500

    Skip Day 23 in runall

--- src/runall.sh
+++ src/runall.sh
@@ -1,4 +1,7 @@
 for i in $(seq -f "%02g" 1 25) ; do
+  if [ "23" = "${i}" ]
+  then echo "Day ${i} --- SKIPPED"; continue
+  fi
   if [ -e "aoc${i}.v" ]
   then echo "Day ${i}"; ./a.sh aoc${i}.v aoc${i}_input.txt solve12
   fi
