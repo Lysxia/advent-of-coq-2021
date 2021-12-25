61a7009ba7fdf7c2a0d770681a2851d7dd83ae45
Li-yao Xia <lysxia@gmail.com>
2021-12-25 12:35:23 -0500

    runall: Run all

--- src/runall.sh
+++ src/runall.sh
@@ -1,11 +1,9 @@
 for i in $(seq -f "%02g" 1 25) ; do
   if [[ -e "aoc${i}.v" ]] ; then
-    CMD="./a.sh aoc${i}.v aoc${i}_input.txt solve12"
-    if [[ "23" = "${i}"  ]]
-    then echo "Day ${i} --- SKIPPED (Part 2 goes OOM) --- ${CMD}"; continue
-    elif [[ "24" = "${i}" ]]
-    then echo "Day ${i} --- SKIPPED (8 minute wait)   --- ${CMD}"; continue
+    echo "Day ${i}"
+    if [[ "24" = "${i}" ]]
+    then echo "This might take 10 minutes."
     fi
-    echo "Day ${i}"; ${CMD}
+    ./a.sh aoc${i}.v aoc${i}_input.txt solve12
   fi
 done
